82d54c19ab3fcd5c12a4768f5340b5b4d0cdc583
carot <caroti.ale@gmail.com>
2019-01-05 15:43:59 -0800

    kufk

--- Laboratorio4 - Testing/ATM 1.0_original/test/EuroTest.java
+++ Laboratorio4 - Testing/ATM 1.0_original/test/EuroTest.java
@@ -1,12 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 class EuroTest {
-
     private Euro e1;
     private final double _e1 = 0.0;
 
@@ -16,8 +15,12 @@ class EuroTest {
     private Euro e3;
     private final double _e3 = 42.0;
 
+    private Euro e;
+    private final double _e = 50000.50;
+
     @BeforeEach
     void init(){
+        e = new Euro(_e);
         e1 = new Euro(_e1);
         e2 = new Euro(_e2);
         e3 = new Euro(_e3);
@@ -30,28 +33,44 @@ class EuroTest {
         assertEquals(e3.getValore(), 4200L);
     }
 
-    @Test
-    void somma() {
-        e1.somma(e2);
-        assertEquals(e1.getValore(),e2.getValore());
+    @ParameterizedTest
+    @ValueSource(doubles = {0.0, 0.50, 500.0, 120.23})
+    void somme(double valToAdd){
+        double initVal        = centToEuro(e.getValore());
+        long  expectedResult = euroToCent(initVal + valToAdd);
 
-        e1.somma(e3);
-        assertEquals(e1.getValore(), e2.getValore()+e3.getValore());
+        e.somma(new Euro(valToAdd));
 
+        assertEquals(e.getValore(), expectedResult);
     }
 
-    @Test
-    void sottrai() {
+    @ParameterizedTest
+    @ValueSource(doubles = {0.0, 0.50, 500.0, 120.23})
+    void sottrai(double valToAdd){
+        double initVal        = centToEuro(e.getValore());
+        long   expectedResult = euroToCent(initVal - valToAdd);
 
+        e.sottrai(new Euro(valToAdd));
+
+        assertEquals(e.getValore(), expectedResult);
     }
 
     @Test
     void ugualeA() {
+        assertTrue(e1.ugualeA(new Euro(_e1)));
+        assertTrue(e2.ugualeA(new Euro(_e2)));
+        assertTrue(e3.ugualeA(new Euro(_e3)));
 
+        assertFalse(e1.ugualeA(e2));
+        assertFalse(e1.ugualeA(e3));
+        assertFalse(e3.ugualeA(e1));
     }
 
     @Test
     void minoreDi() {
+        assertTrue(e1.minoreDi(e2));
+
+        assertTrue(e3.minoreDi(e2));
 
     }
 
@@ -60,5 +79,14 @@ class EuroTest {
         assertEquals(e1.stampa(),"0.0 euro");
         assertEquals(e2.stampa(),"12345.21 euro");
         assertEquals(e3.stampa(),"42.0 euro");
+    }
+
+
+    private double centToEuro(long cent){
+        return (double) cent/100;
+    }
+
+    private long euroToCent(double euro){
+        return (long) (euro*100);
     }
 }
