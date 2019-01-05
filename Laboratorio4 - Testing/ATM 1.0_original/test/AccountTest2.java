428f498acd93946b66ec1f998354523401bb51f9
strafoxultrapowa <aspem2015@gmail.com>
2019-01-05 17:10:39 +0100

    completed accountTester2 class

--- Laboratorio4 - Testing/ATM 1.0_original/test/AccountTest2.java
+++ Laboratorio4 - Testing/ATM 1.0_original/test/AccountTest2.java
@@ -2,12 +2,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.*;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Random;
 
-@RunWith(Parameterized.class)
 class AccountTest2 {
 
     private Account account;
@@ -17,15 +16,13 @@ class AccountTest2 {
     private Euro totalBalance; // funds available + pending deposits
     private final int RANDBOUND=30000;
 
-    //@Parameterized.Parameters
-    //public static
-
+    @BeforeEach
     private void init(){
         Random rand=new Random();
         this.accountNumber=rand.nextInt(RANDBOUND);
         this.pin=rand.nextInt(RANDBOUND);
-        this.availableBalance=,new Euro(rand.nextInt(RANDBOUND));
-        this.totalBalance=,new Euro(rand.nextInt(RANDBOUND));
+        this.availableBalance=new Euro(rand.nextInt(RANDBOUND));
+        this.totalBalance=new Euro(rand.nextInt(RANDBOUND));
         this.account=new Account(accountNumber,pin,availableBalance,totalBalance);
     }
 
@@ -48,14 +45,24 @@ class AccountTest2 {
     }
 
     @ParameterizedTest
-
-    void credit() {
-        //assertEquals(totalBalance.somma());
+    @ValueSource(doubles = {0.0, 0.50, 500.0, 120.23,-100.0})
+    void credit(double toAdd) {
+        Euro eurToAdd=new Euro(toAdd);
+        account.credit(eurToAdd);
+        assertEquals(totalBalance.somma(eurToAdd).getValore(),account.getTotalBalance().getValore());
 
     }
 
-    @Test
-    void debit() {
+    @ParameterizedTest
+    @ValueSource(doubles = {0.0, 0.50, 500.0, 120.23,-100.0})
+    void debit(double toDebit) {
+        Euro eur=new Euro(toDebit);
+        account.debit(eur);
+        totalBalance.sottrai(eur);
+        availableBalance.sottrai(eur);
+        assertEquals(account.getAvailableBalance().getValore(),availableBalance.getValore());
+        assertEquals(account.getTotalBalance().getValore(),totalBalance.getValore());
+
     }
 
     @Test
